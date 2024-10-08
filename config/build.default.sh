73225a01f7452b232ab8ab874b23e11927bfb3f5
qwreey <me@qwreey.moe>
2024-10-08 10:30:42 +0900

    Add unzip, zip command

--- config/build.default.sh
+++ config/build.default.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pacman -Suy cloc less supervisor openssh vim git base-devel cmake gdu btop bash zsh --noconfirm
+pacman -Suy cloc less supervisor openssh vim git base-devel cmake gdu btop bash zsh unzip zip --noconfirm
