58f370d5887f5b095b461aacb55742035e6a94ef
qwreey <me@qwreey.moe>
2024-10-08 14:52:40 +0900

    Fix sshd init error

--- config/user.default.sh
+++ config/user.default.sh
@@ -2,4 +2,6 @@
 
 export HOME="/code"
 
+cp -rn /etc/default/ssh/* /etc/ssh
+ssh-keygen -A
 curl https://raw.githubusercontent.com/qwreey/zsh/master/install.zsh | ZSHFNVM="true" ZSHNVM="true" ZSHPYENV="true" ZSHRUSTUP="true" ZSHNOEXEC="true" ZSHDIR="/code/.zsh" zsh 
