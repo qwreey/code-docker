0c66dc844dcba938cb1944490424b2210e1879aa
qwreey <me@qwreey.moe>
2024-10-08 17:22:55 +0900

    sshd fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 mkdir -p /code/.local
-mkdir -p /code/.local/share/sshd
-ln -s /code/.local/share/sshd /etc/ssh
 if [ -e /install/supervisord.override.conf ]; then
     exec /sbin/supervisord -n -c /install/supervisord.override.conf --user root
 else
