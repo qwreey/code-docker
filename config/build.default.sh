33a0ce008dd2d8a2c474d0a81ea9b64f52922886
Qwreey <me@qwreey.moe>
2024-10-19 18:57:54 +0900

    Update build.default.sh

--- config/build.default.sh
+++ config/build.default.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pacman -Suy cloc less supervisor openssh vim git base-devel cmake gdu btop bash zsh unzip zip --noconfirm
+pacman -Suy gdb cloc less supervisor openssh vim git base-devel cmake gdu btop bash zsh unzip zip --noconfirm
