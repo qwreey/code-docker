cb3dbbd5e5d9263e6a058b3cc147d1634d790acb
qwreey <me@qwreey.moe>
2026-08-01 18:44:06 +0900

    feat: add xdg runtime dir for some extensions

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# Init runtime dir
+mkdir -p /run/xdg && chmod 700 /run/xdg
+export XDG_RUNTIME_DIR=/run/xdg
+
+# Run supervisord
 mkdir -p /code/.local
 if [ -e /etc/code-docker/supervisord.override.conf ]; then
     exec /sbin/supervisord -n -c /etc/code-docker/supervisord.override.conf --user root
